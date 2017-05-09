91254cad9c2dbdcedc1940be5b054dc99f12a695
Dave Strus <dave@getfretless.com>
2017-05-09 02:14:24 -0400

    Add action buttons

--- src/Thing.js
+++ src/Thing.js
@@ -1,5 +1,18 @@
 import React from 'react'
 
+const Actions = ({ thing }) => {
+  return (
+    <span className="actions">
+      <button className="edit">
+        <i className="fa fa-pencil"></i>
+      </button>
+      <button className="remove">
+        <i className="fa fa-trash-o"></i>
+      </button>
+    </span>
+  )
+}
+
 const Thing = ({ thing, saveThing }) => {
   const toggleChecked = (ev) => {
     thing.completed = ev.target.checked
@@ -9,7 +22,11 @@ const Thing = ({ thing, saveThing }) => {
   return (
     <li className="thing">
       <input type="checkbox" defaultChecked={thing.completed} onChange={toggleChecked} />
-      <p>{thing.name}</p>
+      <div className="details">
+        <span className="name">{thing.name}</span>
+        {' '}
+        <Actions />
+      </div>
     </li>
   )
 }
