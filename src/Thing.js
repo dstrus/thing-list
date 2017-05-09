fbd9d24d0b6f3eb723c1d13bb689d437f9310a18
Dave Strus <dave@getfretless.com>
2017-05-09 02:36:06 -0400

    Edit things

--- src/Thing.js
+++ src/Thing.js
@@ -1,21 +1,23 @@
 import React from 'react'
 
-const Actions = ({ thing, removeThing }) => {
-  return (
+const Actions = ({ thing, removeThing }) => (
     <span className="actions">
-      <button className="edit">
-        <i className="fa fa-pencil"></i>
-      </button>
       <button className="remove" onClick={() => removeThing(thing)}>
         <i className="fa fa-trash-o"></i>
       </button>
     </span>
-  )
-}
+)
 
 const Thing = ({ thing, saveThing, removeThing }) => {
+  let name = thing.name
+
   const toggleChecked = (ev) => {
     thing.completed = ev.target.checked
+    saveThing(thing)
+  }
+
+  const updateName = (ev) => {
+    thing.name = ev.target.textContent
     saveThing(thing)
   }
 
@@ -23,7 +25,7 @@ const Thing = ({ thing, saveThing, removeThing }) => {
     <li className="thing">
       <input type="checkbox" defaultChecked={thing.completed} onChange={toggleChecked} />
       <div className="details">
-        <span className="name">{thing.name}</span>
+        <span className="name" contentEditable onBlur={updateName}>{thing.name}</span>
         {' '}
         <Actions thing={thing} removeThing={removeThing}  />
       </div>
